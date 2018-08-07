e08ec928e03292d383c61a26a6539e553b8b584f
Claycorp <Claycorp@doubledoordev.net>
2018-08-06 22:35:06 -0500

    No more blind mod uploading.... Must test everything.

--- src/main/java/net/doubledoordev/burningtorch/BurningTorch.java
+++ src/main/java/net/doubledoordev/burningtorch/BurningTorch.java
@@ -35,7 +35,7 @@ public class BurningTorch
 
     public static final String MOD_ID = "burningtorch";
     public static final String MOD_NAME = "Burning Torch";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
 
     /**
      * This is the instance of your mod as created by Forge. It will never be null.
