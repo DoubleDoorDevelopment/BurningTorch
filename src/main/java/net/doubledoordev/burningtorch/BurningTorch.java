0957afba09855492fa34de9e58ca5e8104c9ad8c
Claycorp <Claycorp@doubledoordev.net>
2018-08-07 22:06:17 -0500

    Fixed some missing textures and possible missing texture when crafting(hopefully).

--- src/main/java/net/doubledoordev/burningtorch/BurningTorch.java
+++ src/main/java/net/doubledoordev/burningtorch/BurningTorch.java
@@ -35,7 +35,7 @@ public class BurningTorch
 
     public static final String MOD_ID = "burningtorch";
     public static final String MOD_NAME = "Burning Torch";
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
 
     /**
      * This is the instance of your mod as created by Forge. It will never be null.
